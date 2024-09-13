e3dd4fd43f1c2d0c69e5c89eb717a2f71bc30fb0
Nikhil Menghani <nikhil@menghani.com>
2024-09-13 19:12:42 -0400

    Add functions.sh and run.sh

---
+++ functions.sh
@@ -0,0 +1,79 @@
+# functions.sh
+
+# Fetch environment variables if necessary
+fetch_env_vars() {
+    if [ -z "$GITLAB_TOKEN" ]; then
+        echo "GITLAB_TOKEN is not set"
+        exit 1
+    fi
+
+    CLONE_DIR="/myenv"
+
+    echo "Cloning the private repository..."
+    git clone https://oauth2:$GITLAB_TOKEN@gitlab.com/nikgapps/myenv.git $CLONE_DIR
+
+    cd $CLONE_DIR
+    pip install -r requirements.txt
+    eval "$(python3 main.py)"
+    cd ..
+}
+
+# Function to check if a string is base64 encoded
+is_base64() {
+    echo "$1" | base64 --decode >/dev/null 2>&1
+    return $?
+}
+
+# Function to set up SSH keys
+setup_ssh() {
+    # Check if the SSH key is base64 encoded
+    if is_base64 "$1"; then
+        DECODED_SSH_KEY=$(echo "$1" | base64 --decode | tr -d '\r')
+        echo "SSH_PRIVATE_KEY was base64 encoded and has been decoded."
+    else
+        DECODED_SSH_KEY=$(echo "$1" | tr -d '\r')
+        echo "SSH_PRIVATE_KEY was not base64 encoded and has been used directly."
+    fi
+    mkdir -p ~/.ssh
+    chmod 700 ~/.ssh
+    which ssh-keyscan || (apt-get update && apt-get install -y openssh-client)
+    eval "$(ssh-agent -s)"
+    echo "$DECODED_SSH_KEY" | ssh-add -
+    ssh-keyscan -H frs.sourceforge.net >>~/.ssh/known_hosts || echo "Failed to scan frs.sourceforge.net"
+    ssh-keyscan -H github.com >>~/.ssh/known_hosts || echo "Failed to scan github.com"
+    ssh-keyscan -H gitlab.com >>~/.ssh/known_hosts || echo "Failed to scan gitlab.com"
+    chmod 600 ~/.ssh/known_hosts
+    echo "SSH setup complete."
+}
+
+setup_git() {
+    git config --global user.email "$USER_EMAIL"
+    git config --global user.name "$USER_NAME"
+    git config --global http.postBuffer 1048576000
+}
+
+get_base_name() {
+    echo $(for f1 in ${1//\// }; do [[ "$f1" == *.zip ]] && echo "$f1" && break; done)
+}
+
+extract_dat_br() {
+    echo "Extracting $1.new.dat.br and converting to $1.img..."
+    brotli -d $1.new.dat.br -o $1.new.dat
+    python /sdat2img/sdat2img.py $1.transfer.list $1.new.dat $1.img
+    rm -rf $1.new.dat.br $1.new.dat $1.transfer.list
+    echo "Extraction of $1.new.dat.br complete."
+    extract_img_7z $1
+}
+
+extract_img_7z() {
+    if [ -f $1.img ]; then
+        FILE_TYPE=$(file "$1.img")
+        echo "Extracting $1.img with type $FILE_TYPE..."
+        mkdir -p "$1"
+        echo "Trying to extract $1.img using 7z."
+        7z x "$1.img" -y -o"$1" >/dev/null 2>&1 || echo "Failed to extract $1.img using 7z."
+        echo "Extraction of $1.img complete."
+    else
+        echo "$1.img not found."
+    fi
+}
---
+++ run.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -e
+
+source /venv/bin/activate
+source functions.sh
+
+fetch_env_vars
+setup_ssh "$SSH_PRIVATE_KEY"
+setup_git
+
+primary_repo="oemdumps"
+git clone git@github.com:nikgapps/$primary_repo.git
+cd $primary_repo
+pip3 install -r requirements.txt
+echo "Downloading $URL..."
+python3 download.py --download $URL
+FILE=$(get_base_name $URL)
+echo "File name is $FILE"
+EXTENSION=$(echo ${FILE##*.} | inline-detox)
+UNZIP_DIR=${FILE/.$EXTENSION/}
+
+mkdir -p "$UNZIP_DIR"
+
+partition_list="system product system_ext"
+# partition_list="system_ext"
+
+if unzip -l "$FILE" | grep -q "payload.bin"; then
+    echo "Extracting payload.bin from the zip file..."
+    unzip -j "$FILE" "payload.bin" -d "$UNZIP_DIR"
+    echo "Extracting payload.bin..."
+    python3 payload_dumper.py "$UNZIP_DIR/payload.bin" --images "$partition_list" --out "$UNZIP_DIR"
+    rm -rf "$UNZIP_DIR/payload.bin"
+else
+    for p in $partition_list; do
+        if unzip -l "$FILE" | grep -q "$p.new.dat.br"; then
+            IMG_FILES="$p.new.dat.br $p.transfer.list"
+            for img_file in $IMG_FILES; do
+                echo "Extracting $img_file from the zip file..."
+                unzip -j "$FILE" "$img_file" -d "$UNZIP_DIR"
+            done
+        else
+            echo "$p.new.dat.br not found in the zip file."
+        fi
+    done
+fi
+
+echo "Extraction complete. Files extracted to $UNZIP_DIR"
+echo "-----------------------------------------------"
+ls -R
+echo "-----------------------------------------------"
+cd $UNZIP_DIR
+for p in $partition_list; do
+    if [ -f "$p.new.dat.br" ]; then
+        extract_dat_br $p
+    elif [ -f "$p.img" ]; then
+        echo "$p.img found."
+        extract_img_7z "$p"
+    else
+        echo "$p.new.dat.br or $p.img not found."
+    fi
+done
+echo "-----------------------------------------------"
+ls -R
+echo "-----------------------------------------------"
+cd ..
