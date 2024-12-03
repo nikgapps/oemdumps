7a8d041bac5bf83b014d07b0849aceb9c7e23592
Nikhil Menghani <nikhil@menghani.com>
2024-12-03 14:59:32 -0500

    Fetch the latest download link of specified google device

--- downloadlink.py
+++ downloadlink.py
@@ -1,6 +1,37 @@
+import argparse
+
 from niklibrary.oem.OemOp import OemOp
 
 device_name = "caiman"
-print(OemOp.get_latest_ota_url(device_name))
+google_devices = {
+    "panther": "Pixel 7",
+    "cheetah": "Pixel 7 Pro",
+    "lynx": "Pixel 7a",
+    "shiba": "Pixel 8",
+    "husky": "Pixel 8 Pro",
+    "akita": "Pixel 8a",
+    "felix": "Pixel Fold",
+    "tangorpro": "Pixel Tablet",
+    "tokay": "Pixel 9",
+    "caiman": "Pixel 9 Pro",
+    "komodo": "Pixel 9 Pro XL",
+    "comet": "Pixel 9 Pro Fold"
+}
 
-# https://dl.google.com/dl/android/aosp/caiman-ota-ad1a.240905.004-36913b8a.zip
+parser = argparse.ArgumentParser(description='OTA payload dumper')
+parser.add_argument('--download', default="caiman", help='folder to read from')
+args = parser.parse_args()
+url = args.download
+
+# Convert the dictionary to lower case for case-insensitive comparison
+google_devices_lower = {k.lower(): v.lower() for k, v in google_devices.items()}
+
+if url.lower() in google_devices_lower.keys():
+    url = OemOp.get_latest_ota_url(url.lower())
+elif url.lower() in google_devices_lower.values():
+    device_name = list(google_devices_lower.keys())[list(google_devices_lower.values()).index(url.lower())]
+    url = OemOp.get_latest_ota_url(device_name)
+else:
+    url = args.download
+
+print(url)
